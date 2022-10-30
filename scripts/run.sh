1e182b1e3e51ceb79bebe9f34d80226125c922da
Hans-Joerg Peter <hans-joerg.peter@siemens.com>
2022-10-30 03:07:43 -0700

    Add check whether the cppdev Docker image actually exist to scripts/run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-dir=`pwd`
-user=`whoami`
+image="cppdev"
+if [[ "$(docker images -q $image 2> /dev/null)" == "" ]]; then
+    echo "You first need to create the $image image; see README.md for details"
+    exit 1
+fi
 
-docker run -it --rm -v $dir:/out:rw cppdev "$@"
+dir=`pwd`
+docker run -it --rm -v $dir:/out:rw $image "$@"
