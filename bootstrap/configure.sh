0f38ce5cfa64a44eadca044c84bfa65080846161
Hans-Joerg Peter <hans-joerg.peter@siemens.com>
2022-11-04 08:32:19 -0700

    Fix the parsing of the build type from command-line argument in configure.sh

--- bootstrap/configure.sh
+++ bootstrap/configure.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 build_type="Release"
-if [ "$#" -ge 2 ]; then
-    build_type="$2"
+if [ "$#" -ge 1 ]; then
+    build_type="$1"
 fi
+echo "Running CMake configure for $build_type"
 
 cmake  -B build  -S .  -DCMAKE_BUILD_TYPE=$build_type  -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
